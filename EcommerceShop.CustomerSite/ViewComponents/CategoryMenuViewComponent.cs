b3f182bfc60b970911d00361ecf13ba0b993b57a
Nguyễn Lê Nguyên <nglenguyenn@gmail.com>
2021-07-16 14:31:18 +0700

    Add category menu

---
+++ EcommerceShop.CustomerSite/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceShop.CustomerSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceShop.CustomerSite.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ICategoryApiClient _categoryApiClient;
+
+        public CategoryMenuViewComponent(ICategoryApiClient categoryApiClient)
+        {
+            _categoryApiClient = categoryApiClient;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categorys = await _categoryApiClient.GetCategories();
+
+            return View(categorys);
+        }
+    }
+}
