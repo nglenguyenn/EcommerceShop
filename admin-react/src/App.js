f9f1402c8cf632523eaf8face807f06756c31615
Nguyễn Lê Nguyên <nglenguyenn@gmail.com>
2021-07-19 11:42:33 +0700

    Admin Product

--- admin-react/src/App.js
+++ admin-react/src/App.js
@@ -1,5 +1,7 @@
 import Layout from './containers/Layout';
+import Home from './components/Home';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Product from './containers/Product';
 
 import './App.css';
 
@@ -7,7 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Layout>
+        <Route path="/" exact><Home /></Route>
 
+        <Route path="/products" ><Product /></Route>
       </Layout>
     </BrowserRouter>
   );
--- admin-react/src/containers/Layout/index.jsx
+++ admin-react/src/containers/Layout/index.jsx
@@ -1,12 +1,10 @@
 import MenuTop from "../MenuTop";
-import Home from "../../components/Home/Home";
 
 function Layout({children}) {
 
      return (
          <>
            <MenuTop></MenuTop>
-           <Home></Home>
            {children}
          </>
      );
